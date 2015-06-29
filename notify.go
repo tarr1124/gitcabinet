327d1607a86a393a7fe848f6540e04ed89cf0506
Takahiro Tokunaga <takahiro.tokunaga@rakuten.com>
2015-06-29 09:08:10 +0000

    add gitpush

--- notify.go
+++ notify.go
@@ -4,7 +4,13 @@ import (
 	"golang.org/x/exp/inotify"
 	"log"
 	"syscall"
+	"os"
+	"os/exec"
+	"fmt"
 )
+
+
+var targetDir = "/home/tarr/dev/go/diffwatcher/gitcabinet_test_repo"
 
 func main(){
 		watcher, err := inotify.NewWatcher()
@@ -12,7 +18,7 @@ func main(){
 			log.Fatal(err)
 		}
 
-		err = watcher.AddWatch("/tmp", syscall.IN_CREATE)
+		err = watcher.AddWatch(targetDir, syscall.IN_ALL_EVENTS)
 
 		if err != nil {
 			log.Fatal(err)
@@ -22,9 +28,33 @@ func main(){
 			select {
 			case ev := <-watcher.Event:
 				log.Println("event:", ev)
-				log.Println("Name:", ev.Mask)
+				GitPush()
 			case err := <-watcher.Error:
 				log.Println("error:", err)
 			}
 		}
+}
+
+func GitPush() {
+	// change directory to target directory.  
+
+/*
+	out, err := exec.Command("git", "rev-parse", "--show-toplevel").Output()	
+	if err != nil {
+    	fmt.Println(err)
+        os.Exit(1)
+    }
+	target_path := string(out)
+*/
+
+	os.Chdir(targetDir)
+
+	out, err := exec.Command("git", "push", "origin", "master").Output()	
+
+	if err != nil {
+    	fmt.Println(err)
+        os.Exit(1)
+    }
+
+    fmt.Println(string(out))
 }
